3e8b99a8b700d5df87a391955d0907de968c9313
ThatsGreatDude <malenest@hiof.no>
2025-04-20 10:22:38 +0200

    error localhost
    Error på localhost som sa at embedded sass ikke var der, installerte det og det gikk vekk. Vanskelig å jobbe uten en fungerende sanity, satt inn en mockup.
    
    Nå er error borte fra localhost, men det er error i consoll og ingenting vises akkurat nå

--- gruppe_40_uin/src/components/Header.jsx
+++ gruppe_40_uin/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { client } from '../sanityClient';
+import { mockMembers } from '../mockMembers'; // Bruk mock i stedet for Sanity
 import './Header.scss';
 
 const Header = () => {
